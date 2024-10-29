c0bd9c15774d787f8a0af8b9da0726d1c839088f
SORO ソロ <44982539+RIL75@users.noreply.github.com>
2024-10-29 08:55:20 +0100

    bonus 3

--- Python/60_days_20 apps_udemy/day_03/main.py
+++ Python/60_days_20 apps_udemy/day_03/main.py
@@ -7,9 +7,11 @@ while True:
         case 'add':
             todo = input("Enter a todo: ")
             todos.append(todo)
-        case 'show':
+        case 'show' | 'display':
             for item in todos:
                 print(item)
         case 'exit':
             break
+        case _:
+            print("Hey, you entered an unknown command")
 print("Bye!")
