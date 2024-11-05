2b3a04376ea64f8eb4ed2877600388c6c5277676
SORO ソロ <44982539+RIL75@users.noreply.github.com>
2024-11-05 12:10:37 +0100

    day6 from 60 is done

--- Python/60_days_20 apps_udemy/day_06/main.py
+++ Python/60_days_20 apps_udemy/day_06/main.py
@@ -1,14 +1,20 @@
-
 while True:
     user_action = input("Type add, show, complete or exit \n ")
     user_action = user_action.strip()
     match user_action:
         case 'add':
             todo = input("Enter a todo: ") + "\n"
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
             todos.append(todo)
             file =open('todos.txt', 'w')
             file.writelines(todos)
+            file.close()
         case 'show' | 'display':
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
             for index, item in enumerate(todos):
                 row = f"{index+1}-{item}"
                 print(row)
