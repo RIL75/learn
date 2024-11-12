862c734cd544f1ef53c18232ad45bb2589654bb2
SORO ソロ <44982539+RIL75@users.noreply.github.com>
2024-11-12 18:38:45 +0100

    Update bonus11.py

--- Python/60_days_20 apps_udemy/bonus/bonus11.py
+++ Python/60_days_20 apps_udemy/bonus/bonus11.py
@@ -12,3 +12,27 @@ average = get_average()
 print(average)
 
 
+############################################
+#exercise
+###########################################
+
+def format_filename():
+    filename = "report.txt"
+    filename = filename[:-4].capitalize()
+    return filename
+print(format_filename())
+
+
+
+########################################
+
+def get_maximum():
+    celsius = [14, 15.1, 12.3]
+    maximum = max(celsius)
+    return (maximum)
+
+
+celsius = get_maximum()
+fahrenheit = celsius * 1.8 + 32
+
+print(fahrenheit)
