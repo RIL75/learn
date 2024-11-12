991318003c3253306de81e1753b0df79482fef35
SORO ソロ <44982539+RIL75@users.noreply.github.com>
2024-11-12 12:19:23 +0100

    Passwort_Generator_v1_initial

---
+++ Python/Python_100_days_udemy/day_5 - Password Generator/Password_Generator_v1.py
@@ -0,0 +1,21 @@
+#Day 05 - Password Generator Excersize
+import random
+
+from Password_Generator_v2 import nr_letters
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'response', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+password_list =[]
+for char in range(9, nr_letters):
+    password_list.append(random.choice(letters))
+
+
+
+
+
+
+password = ''.join(map(str, final_list))
+print(f"Your password is: {password}")
